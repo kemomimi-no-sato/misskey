c6910ed53d20b99f5b7c5a0505f02ae5c472bcfd
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-11-20 22:27:28 +0900

    change: リアクションできないものにはメニューを出さないように

--- packages/frontend/src/components/global/MkCustomEmoji.vue
+++ packages/frontend/src/components/global/MkCustomEmoji.vue
@@ -78,7 +78,7 @@ const alt = computed(() => `:${customEmojiName.value}:`);
 const errored = ref(url.value == null);
 
 function onClick(ev: MouseEvent) {
-	if (props.menu) {
+	if (props.menu && canReact.value) {
 		os.popupMenu([{
 			type: 'label',
 			text: `:${props.name}:`,
@@ -89,7 +89,7 @@ function onClick(ev: MouseEvent) {
 				copyToClipboard(`:${props.name}:`);
 				os.success();
 			},
-		}, ...(props.menuReaction && canReact.value && react ? [{
+		}, ...(props.menuReaction && react ? [{
 			text: i18n.ts.doReaction,
 			icon: 'ti ti-plus',
 			action: () => {
