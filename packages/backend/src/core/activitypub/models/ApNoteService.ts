2c81ac002bc7f57836b5e3917bd923391bd43673
HanasakiNayori <hibariruri@gmail.com>
2025-11-18 19:12:43 +0900

    定義抜けの修正

--- packages/backend/src/core/activitypub/models/ApNoteService.ts
+++ packages/backend/src/core/activitypub/models/ApNoteService.ts
@@ -222,6 +222,14 @@ export class ApNoteService {
 			}
 		}
 
+		const files: MiDriveFile[] = [];
+
+		for (const attach of toArray(note.attachment)) {
+			attach.sensitive ??= note.sensitive;
+			const file = await this.apImageService.resolveImage(actor, attach);
+			if (file) files.push(file);
+		}
+
 		// リプライ
 		const reply: MiNote | null = note.inReplyTo
 			? await this.resolveNote(note.inReplyTo, { resolver })
